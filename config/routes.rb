7993b1de57e20381977dade7f5e1b19e862e1b60
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-11-18 18:30:57 +0100

    routes vertifié Kevin

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show] do
+    post 'favorites', to: 'restaurants#toggle_favorites'
+  end
+
+  resources :favorites, only: [:index]
+  resources :reviews, only: [:new, :create, :index] #index for my_reviews
+  resource :profile, only: :show
+  resources :coupons, only: :index
+
+  namespace :manager do
+    resources :restaurants, only: [:new, :create, :edit, :update, :destroy, :index, :show]
+  end
 end
