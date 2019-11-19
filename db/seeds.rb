b577208229f23c6c6546be74170619c5b1599f6d
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2019-11-19 11:18:38 +0100

    debut seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 1) seed des restos
+ ChezPaulBocuse = Restaurant.new (name: 'Paul Bocuse',
+    type: "Gastronomique",
+    address: "Colonges au Mont d'or",
+    description: "Le Restaurant du célèbre chef étoilé Lyonnais Paul Bocuse",
+    pricing: "de 75 a 120€ le menu",
+    rating: 4,
+    phone_number: "04-12-34-56-78")
+
+  McdoBellecours = Restaurant.new (name: 'Mcdo de la place Bellecours',
+    type: "Fast-food",
+    address: "Place bellecours, Lyon",
+    description: "Un des nombreux restaurants de la chaine",
+    pricing: "de 2 a 12€",
+    rating: 3,
+    phone_number: "04-12-34-56-78")
+
+
+  ChezNino = Restaurant.new (name: 'Chez Nino',
+    type: "Italien",
+    address: "Place Bellecours, Lyon",
+    description: "Pizza et auttres spécialités italiennes au menu dans une ambiance agréable a 2 pas de la place bellecours",
+    pricing: "de 10 a 20€ le menu",
+    rating: 4,
+    phone_number: "04-12-34-56-78")
+
+
+
+# 2) seed des users
+John = User.new (
+email: "john@hotmail.fr",
+  )
+
+# 3) seed des reviews
