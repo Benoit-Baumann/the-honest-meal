a24a2fc5901740eb2a3c89350ce44bbf43ba2d33
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2019-11-21 17:52:53 +0100

    rajout des graphes dans la page coupon, rajout de la gemme chartjs_ror

---
+++ app/javascript/components/chart.js
@@ -0,0 +1,54 @@
+import Chart from 'chart.js';
+
+// var ctx = document.getElementById('myRate');
+// var myRate = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+//         datasets: [{
+//             label: 'Notes',
+//             data: [3, 3.4, 3, 4.2, 3.7, 3, 3.2, 3.5, 3.3, 3.7, 4, 3.4],
+//             backgroundColor: 'rgb(143, 214, 148)',
+//             borderColor:'rgb(143, 214, 148)',
+//             borderWidth: 1,
+//             fill: false
+//         }]
+//     },
+//     options: {
+//             responsive: true,
+//             maintainAspectRatio: false,
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
+
+// var ctx = document.getElementById('surveysSent');
+// var surveysSent = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin','Juillet','Aout','Septembre','Octobre','Novembre','Decembre'],
+//         datasets: [{
+//             label: '# de sondages envoyés',
+//             data: [300, 257, 312, 420, 371, 305, 382, 375, 333, 307, 403, 324],
+//             backgroundColor: 'rgba(143, 214, 148, 0.4)',
+//             borderColor:'rgba(143, 214, 148, 0.4)',
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//             responsive: true,
+//             maintainAspectRatio: false,
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
