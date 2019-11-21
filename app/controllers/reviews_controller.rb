7ae267ca7a621115106f90e32e7a49db1d0ddaf2
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-11-21 16:19:25 +0100

    t

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,28 @@
 class ReviewsController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
-    def index
-        @reviews = current_user.reviews
-    end
+  def index
+    @reviews = current_user.reviews
+  end
 
-    def new
-        @review = Review.new
-    end
+  def new
+    @review = Review.new
+  end
 
-    def create
-        @review = Review.new(review_params)
-        @review.user = current_user
-        if @review.save
-            redirect_to profile_path
-        else
-            render :new
-        end
-    end
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.save!
+  #   if @review.save
+  #     redirect_to profile_path
+  #   else
+  #     render :new
+  #   end
+  end
 
-    private
+  private
 
-    def review_params
-        params.require(:review).permit(:user, :restaurant, :username, :content, :rating)
-    end
+  def review_params
+    params.require(:review).permit(:user, :restaurant, :username, :content, :rating)
+  end
 end
