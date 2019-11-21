e05b653bcd073f72457c5f02af1ee6cb59afcdd7
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2019-11-21 17:52:53 +0100

    rajout des graphes dans la page coupon, rajout de la gemme chartjs_ror

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
 //= require rails-ujs
 //= require_tree .
+<<<<<<< HEAD
 
+=======
+//= require Chart.min
+>>>>>>> a24a2fc... rajout des graphes dans la page coupon, rajout de la gemme chartjs_ror
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import "bootstrap";
+<<<<<<< HEAD
 import initLoadingAnimation from './loading_animation';
 import initStarRating from '../plugins/init_star_rating';
 
 initStarRating();
 
 initLoadingAnimation();
+=======
+import '../components/chart';
+>>>>>>> a24a2fc... rajout des graphes dans la page coupon, rajout de la gemme chartjs_ror
