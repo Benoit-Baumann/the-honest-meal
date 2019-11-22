b0e537311ae575b079568b2f9857aa05b23abd68
Norberg <a.norberg91@gmail.com>
2019-11-22 14:21:10 +0100

    dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,13 @@
 import "bootstrap";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import initLoadingAnimation from './loading_animation';
 import initStarRating from '../plugins/init_star_rating';
+=======
+import '../components/chart';
+import '../components/drag-drop';
+import { initStarRating } from '../plugins/init_star_rating';
+>>>>>>> 49388bf... dashboard
 
 initStarRating();
 
