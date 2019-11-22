85e1570e9b0deebcc63e6d71ca4b23e6bd649440
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-22 17:08:12 +0100

    Login modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
 import "bootstrap";
 import initLoadingAnimation from './loading_animation';
+import initTabListener from './modal_login_form';
 import initStarRating from '../plugins/init_star_rating';
 import '../components/chart';
 import '../components/drag-drop';
 
 initStarRating();
+initTabListener();
 
 //initLoadingAnimation();
