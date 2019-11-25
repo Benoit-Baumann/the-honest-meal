19fc7122ddba3c62fb17dc5db6c02bfbfd5c220a
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-11-25 11:50:08 +0100

    finish choice of text ou rating

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,14 @@
 import "bootstrap";
 import initLoadingAnimation from './loading_animation';
 import initTabListener from './modal_login_form';
-import initStarRating from '../plugins/init_star_rating';
+import {initStarRating, initStarRating2} from '../plugins/init_star_rating';
 import initNavbarButtons from './navbar_active_button'
 import '../components/chart';
 import '../components/drag-drop';
 
 initNavbarButtons();
 initStarRating();
-initTabListener();
+initStarRating2();
+// initTabListener();
 
 //initLoadingAnimation();
