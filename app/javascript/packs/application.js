9119aa230d5497fb5b7c2700b15242a60dd7b20d
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-25 22:28:33 +0100

    Fix login modal tab bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import "bootstrap";
-import initTabListener from './modal_login_form';
-import {initStarRating, initStarRating2} from '../plugins/init_star_rating';
+import { initTabListener, initJoinUsBtn} from './modal_login_form';
+import { initStarRating, initStarRating2 } from '../plugins/init_star_rating';
 import '../components/drag-drop';
 
 initStarRating();
 initStarRating2();
 initTabListener();
+initJoinUsBtn();
