bb522cc094432b7b52c2645251796402d9bcf6e9
Julien GREGOIRE <jgregoire9244@hotmail.fr>
2019-11-25 10:51:06 +0100

    disparition du login successfully après 10s

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,18 @@ import initNavbarButtons from './navbar_active_button'
 >>>>>>> 1eeece4... Login modal now functional
 import '../components/chart';
 import '../components/drag-drop';
+<<<<<<< HEAD
 import { initStarRating } from '../plugins/init_star_rating';
 >>>>>>> 49388bf... dashboard
+=======
+import '../components/hide';
+>>>>>>> 5a3b520... disparition du login successfully après 10s
 
 initNavbarButtons();
 initStarRating();
 initTabListener();
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 initLoadingAnimation();
 =======
@@ -26,3 +31,6 @@ import '../components/chart';
 =======
 //initLoadingAnimation();
 >>>>>>> 888f1d2... modif dashboard avec ajout charts
+=======
+//initLoadingAnimation();
+>>>>>>> 5a3b520... disparition du login successfully après 10s
