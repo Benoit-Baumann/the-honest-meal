4cf5ebf21b2ec52ca8f7e3254c2c6de284455b01
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-11-26 18:57:37 +0100

    t

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :restaurant
-  has_many :answers
 
   before_validation :generate_token
 
   # validates :user, presence: true
-  validates :username, uniqueness: { case_sensitive: false }, presence: true, unless: :user_id?, on: :update #Username requis si user_id absent (user non connecté ou ne possédant pas de compte)
+  validates :username, presence: true, unless: :user_id?, on: :update #Username requis si user_id absent (user non connecté ou ne possédant pas de compte)
   validates :restaurant, presence: true
   validates :content, presence: true, length: { maximum: 1000 }, on: :update
   validates :content_title, presence: true, length: { maximum: 25 }, on: :update
@@ -21,5 +20,4 @@ class Review < ApplicationRecord
       break random_token unless Review.exists?(token: random_token)
     end
   end
-  
 end
