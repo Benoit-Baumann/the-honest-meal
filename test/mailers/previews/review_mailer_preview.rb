456805da75713ef5b916f32fcd5f218aa35d956d
Norberg <a.norberg91@gmail.com>
2019-11-27 11:48:51 +0100

    modif mail

--- test/mailers/previews/review_mailer_preview.rb
+++ test/mailers/previews/review_mailer_preview.rb
@@ -2,8 +2,7 @@
 class ReviewMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/review_mailer/welcome
-  def welcome
-    ReviewMailer.welcome
+  def send_link
+    ReviewMailer.with(review: Review.last, email: "toto@gmail.com").send_link
   end
-
 end
