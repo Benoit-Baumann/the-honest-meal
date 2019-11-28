fb8f260caa5bafd1f20153f605d8f74326ab5439
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-28 13:32:41 +0100

    Add style to question type btn on edit questions page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,13 @@
 import "bootstrap";
 import { initTabListener, initMainSignupBtnListener, initSendReviewBtnListener } from './modal_listener';
 import { initStarRating, initStarRating2 } from '../plugins/init_star_rating';
-import '../components/drag-drop';
+// import '../components/drag-drop';
 import '../components/showqs';
+import { initQuestionTypeBtnListener } from "./question_type_btn_listener";
 
 initStarRating();
 initStarRating2();
 initTabListener();
 initMainSignupBtnListener();
 initSendReviewBtnListener();
+initQuestionTypeBtnListener();
