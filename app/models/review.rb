33fdae96fbccf256d8eb5c29f8f9b5eef24efac5
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-28 15:38:34 +0100

    Add css style

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,7 +8,7 @@ class Review < ApplicationRecord
   validates :username, presence: true, unless: :user_id?, on: :update #Username requis si user_id absent (user non connecté ou ne possédant pas de compte)
   validates :restaurant, presence: true
   validates :content, presence: true, length: { maximum: 1000 }, on: :update
-  validates :content_title, presence: true, length: { maximum: 25 }, on: :update
+  validates :content_title, presence: true, length: { maximum: 40 }, on: :update
   validates :rating, presence: true, inclusion: { in: 0..5 }, on: :update
 
   private
