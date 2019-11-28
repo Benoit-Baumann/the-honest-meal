0afa7c690cda27f9c337d1642a00a18c4d67deab
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-28 18:22:54 +0100

    Change pie chart to doughnut chart

--- db/seeds.rb
+++ db/seeds.rb
@@ -278,12 +278,13 @@ end
     else
       review.username = Faker::Internet.username
     end
-      updated_date = rand(Date.civil(2019, 1, 1)..Date.civil(2019, 12, 31))
+    review.save!
     # Randomize if a review is answered by a customer (with true_ratio = 0.91)
-    if Faker::Boolean.boolean(true_ratio: 0.2)
-      created_date = updated_date - (rand(1..7))
+    review.created_at = rand(Date.civil(2019, 1, 1)..Date.civil(2019, 11, 29))
+    if Faker::Boolean.boolean(true_ratio: 0.88)
+      review.updated_at = review.created_at + (rand(1..7))
     else
-      create_date = updated_date
+      review.updated_at = review.created_at
     end
     review.save!
     # Randomize if a review is answered by a customer (with true_ratio = 0.91)
