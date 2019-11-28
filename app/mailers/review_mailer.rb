bec1461f856a3c775465126a404faaed6021b671
Benoit Baumann <baumann.benoit@gmail.om>
2019-11-28 10:01:53 +0100

    Configure ActionMailer with sendgrid

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -2,7 +2,6 @@ class ReviewMailer < ApplicationMailer
   def send_link
     @email = params[:email]
     @review = params[:review]
-    # @review_link = "http://localhost:3000/reviews/#{@review.token}/edit"
     mail(to: @email, subject: 'Laisser votre avis !')
   end
 end
